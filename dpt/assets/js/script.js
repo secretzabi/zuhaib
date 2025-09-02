715e808158dc171f2484a279297e4b763b6c521a
zuhaibhassan <zuhaibhassan05@gmail.com>
2025-09-02 12:11:37 +0500

    dpt all pages added

--- dpt/assets/js/script.js
+++ dpt/assets/js/script.js
@@ -6,3 +6,30 @@
     const expanded = hamburger.getAttribute("aria-expanded") === "true" || false;
     hamburger.setAttribute("aria-expanded", !expanded);
 });
+
+    function startPracticing(){
+        const savedState = localStorage.getItem('userState');
+
+        if (savedState) {
+        // If we already know the user’s state → go there
+        window.location.href = savedState;
+        return;
+    }
+
+        // Otherwise, fallback to California for first-time visitors
+        window.location.href = 'state-california.html';
+
+        // In the background, try to detect state for next time
+        fetch('https://ipapi.co/json/')
+        .then(r => r.json())
+        .then(d => {
+        const stateMap = {
+        'CA': 'state-california.html',
+        'TX': 'state-texas.html'
+    };
+        if (d.region_code && stateMap[d.region_code]) {
+        localStorage.setItem('userState', stateMap[d.region_code]);
+    }
+    })
+        .catch(() => {});
+    }
