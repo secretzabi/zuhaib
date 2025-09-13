c59c87745b256d92a259189f95a2e1ca79b7fc55
zuhaibhassan <zuhaibhassan05@gmail.com>
2025-09-14 00:23:20 +0500

    implant pages

--- implant-identifier/assets/js/main.js
+++ implant-identifier/assets/js/main.js
@@ -1,14 +1,18 @@
-const hamburger = document.getElementById('hamburger');
-const nav = document.querySelector('nav');
-const closeBtn = document.getElementById('close-btn');
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById('hamburger');
+    const nav = document.querySelector('nav');
+    const closeBtn = document.getElementById('close-btn');
 
-hamburger.addEventListener('click', () => {
-    const expanded = hamburger.getAttribute('aria-expanded') === 'true';
-    hamburger.setAttribute('aria-expanded', !expanded);
-    nav.classList.toggle('active');
-});
+    if (hamburger && nav && closeBtn) {
+        hamburger.addEventListener('click', () => {
+            const expanded = hamburger.getAttribute('aria-expanded') === 'true';
+            hamburger.setAttribute('aria-expanded', !expanded);
+            nav.classList.toggle('active');
+        });
 
-closeBtn.addEventListener('click', () => {
-    nav.classList.remove('active');
-    hamburger.setAttribute('aria-expanded', 'false');
+        closeBtn.addEventListener('click', () => {
+            nav.classList.remove('active');
+            hamburger.setAttribute('aria-expanded', 'false');
+        });
+    }
 });
